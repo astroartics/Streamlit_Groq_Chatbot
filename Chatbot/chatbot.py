220f7bb69c004b521b901f81c366cf6ffe358b59
astroartics <joshisanjanana114@gmail.com>
2024-12-31 02:51:06 +0530

    Chatbot Update

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from PIL import Image
 import time
-import numpy as np
-import random
+# import numpy as np
+# import random
 from groq import Groq
-import json
+from streamlit_js_eval import streamlit_js_eval
 
-st.title("Chatbot")
-st.chat_message("ai").text("Hey there, I'm WIAN - What's In A Name 👋")
 
 message_back = """
 <style>
@@ -16,13 +14,27 @@ message_back = """
 .e10jh26i0 {display : none;}
 header {visibility : hidden;}
 #chatbot {visibility : visible; width : 100%; font-size : 2.5rem; margin : 0; text-align : center; font-weight : 100; position : fixed; top : 0; left : 50%; transform : translate(-50%,0); z-index : 1; background-color : black;}
-.st-emotion-cache-w9jsfw {color : black; font-size : 1.1em;}
+.st-emotion-cache-w9jsfw {color : black;}
 .e1v02oy80 {color : white;}
 .st-emotion-cache-janbn0 {background-color : rgb(115, 115, 115); box-shadow : inset 4px 4px 10px 0 rgba(0,0,0, 0.1), inset -4px -4px 10px 0 rgba(0,0,0, 0.1); border : 1.5px solid rgba(255,255,255,0.5)}
 .st-emotion-cache-4oy321 {background-color : rgb(57, 59, 56); box-shadow : inset 2px 2px 8px 0 rgba(0,0,0, 0.1), inset -2px -2px 8px 0 rgba(0,0,0, 0.1); border : 1.5px solid rgba(255,255,255,0.5)}
-</style>
+.stAlertContainer {padding : 4px 6px 4px 6px; width : fit-content;}
+.e1obcldf2 {padding : 0 7px 0 7px; position : relative; left : 50%; transform : translate(-50%,0); border : 0.2px solid white; background-color : rgb(225, 225, 225);; color : black; box-shadow : inset 1px 1px 10px 0 rgba(0, 0, 0, 0.2), inset -1px -1px 10px 0 rgba(0, 0, 0, 0.2); margin-bottom : 5px;}
+.e1obcldf2:hover {color : white; background-color : rgb(77, 77, 77); border : 0.2px solid white; transition : 0.2s; box-shadow : inset 1px 1px 10px 0 rgba(0, 0, 0, 0.2), inset -1px -1px 10px 0 rgba(0, 0, 0, 0.2);}
+.e1obcldf2:focus:not(:active) {color : white; background-color : rgb(64, 64, 64); border : 0.2px solid rgb(177, 177, 177);}
+.e121c1cl3 {display : none;}
+div[data-testid=stToast] {position : fixed; bottom:10px; right:50%; transform:translate(50%,0); background-color:rgb(69, 69, 69);color:white;}
+</style>   
 """
 st.markdown(message_back,unsafe_allow_html = True)
+
+avatar_ai = Image.open('Ai_avatar.png')
+prompt = st.chat_input("What is up?")
+st.title("Chatbot")
+time.sleep(0.5)
+st.chat_message("assistant",avatar = avatar_ai).text("Hey there! I'm WIAN - What's In A Name 👋")
+time.sleep(0.5)
+st.chat_message("assistant",avatar = avatar_ai).text("What's on your mind today?")
 
 
 
@@ -42,7 +54,7 @@ for message in st.session_state.messages :
     if message['role'] == 'user':
         st.chat_message(message['role'],avatar = avatar_img).text(message['content'])
     else : 
-        st.chat_message(message['role']).text(message['content'])    
+        st.chat_message(message['role'],avatar = avatar_ai).text(message['content'])    
 
 
 
@@ -60,24 +72,42 @@ for message in st.session_state.messages :
 #         time.sleep(0.05)
 
 
+
+def write_response(res) :
+    for letter in res:
+        yield letter + ""
+        time.sleep(0.028)
+
 client = Groq(api_key = st.secrets["GROQ_API_KEY"])
 
-
-if prompt := st.chat_input("What is up?") :
+if prompt :
+    time.sleep(0.15)
     st.session_state.messages.append({"role" : "user",'content' : prompt})
     st.chat_message("user",avatar = avatar_img).text(prompt)
 
     # response = np.random.randn(1)
     # with st.chat_message("assistant"):
     #     response = st.write_stream(response_generator())    # Writing one letter at a time, in the chat_message section of Assistant
-    
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(    
-            model = st.session_state["groq_model"],
-            messages = [
-                {"role" : m["role"], "content" : m["content"]} for m in st.session_state.messages
-            ]
-        )
-        
-        st.write(stream.choices[0].message.content)
-    st.session_state.messages.append({"role" : "assistant","content" : stream.choices[0].message.content})
+    time.sleep(0.08)
+    with st.chat_message("assistant",avatar = avatar_ai):
+        try:
+            stream = client.chat.completions.create(    
+                model = st.session_state["groq_model"],
+                messages = [
+                    {"role" : m["role"], "content" : m["content"]} for m in st.session_state.messages
+                ]
+            )
+            response = st.write_stream(write_response(stream.choices[0].message.content))   
+            st.session_state.messages.append({"role" : "assistant","content" : stream.choices[0].message.content})
+        except Exception as e :
+            st.error("⚠️ Oops! Something went wrong...")     
+            st.session_state.messages.append({"role" : "assistant","content" : "⚠️ Something went wrong"})      
+
+if st.button("Clear Chat"):
+    if not st.session_state.messages:
+        st.toast("Nothing to clear")       
+    else:
+        st.toast("Clearing the chat...")          
+    time.sleep(0.1)
+    st.session_state.clear()
+    st.rerun()
